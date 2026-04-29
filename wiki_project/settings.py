186f1fe9c58008bb16c1e8cabcebc202a9f91b8d
Giovani <gibinha.jesus@gmail.com>
2026-04-29 14:20:41 -0300

    Fix de compatibilidade com Django

--- wiki_project/settings.py
+++ wiki_project/settings.py
@@ -75,13 +75,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 if os.getenv('R2_ACCESS_KEY_ID'):
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.getenv('R2_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('R2_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('R2_BUCKET_NAME')
@@ -91,6 +88,16 @@ if os.getenv('R2_ACCESS_KEY_ID'):
     AWS_DEFAULT_ACL = None
     AWS_S3_FILE_OVERWRITE = False
     MEDIA_URL = f"https://{os.getenv('R2_PUBLIC_URL')}/"
+    STORAGES = {
+        'default': {'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'},
+        'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'},
+    }
+else:
+    MEDIA_URL = '/media/'
+    STORAGES = {
+        'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
+        'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'},
+    }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
