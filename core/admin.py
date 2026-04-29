b38f2c5f0e809dce98a4381116ea230fafc0a260
Giovani <gibinha.jesus@gmail.com>
2026-04-29 16:00:14 -0300

    Criando a model Core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import MajorTopic, MinorTopic, ContentBlock
 
-# Register your models here.
+
+@admin.register(MajorTopic)
+class MajorTopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'order')
+    search_fields = ('title',)
+
+
+@admin.register(MinorTopic)
+class MinorTopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'major_topic', 'order')
+    list_filter = ('major_topic',)
+    search_fields = ('title',)
+
+
+@admin.register(ContentBlock)
+class ContentBlockAdmin(admin.ModelAdmin):
+    list_display = ('minor_topic', 'block_type', 'title', 'order')
+    list_filter = ('block_type', 'minor_topic__major_topic')
